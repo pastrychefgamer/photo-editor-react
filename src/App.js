fba44e024ad6adbd634544c551ed1e8e3e9a509c
pastrychefgamer <52508868+pastrychefgamer@users.noreply.github.com>
2020-09-17 16:56:26 -0700

    imported slider and now page is rendering

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Slider from './Slider'
 
 function App() {
   return (
--- src/Slider.js
+++ src/Slider.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Slider() {
     return (
         <div>
-            
+            Slider
         </div>
     )
 }
